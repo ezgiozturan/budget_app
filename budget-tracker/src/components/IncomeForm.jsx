d295a447265874be8bf89dea0f7fadc5e7c00c1c
ezgiozturan <ezgiozturan05@gmail.com>
2024-11-23 14:52:26 +0300

    Update IncomeForm.jsx
    useState react hooku eklendi

--- budget-tracker/src/components/IncomeForm.jsx
+++ budget-tracker/src/components/IncomeForm.jsx
@@ -1,18 +1,25 @@
+import { useState } from "react";
+
 export default function IncomeForm() {
-  return;
-  <form onSubmit={handleSubmit}>
-    <div>
-      <label>Gelir Açıklaması</label>
-      <input type="text" value={description} placeholder="Örneğin: Maaş" />
-    </div>
-    <div>
-      <label>Miktar</label>
-      <input type="number" value={amount} placeholder="Örneğin: 5000" />
-    </div>
-    <div>
-      <label>Tarih</label>
-      <input type="date" value={date} />
-    </div>
-    <button type="submit">Gelir Ekle</button>
-  </form>;
+  const [description, setDescription] = useState(" ");
+  const [amount, setAmount] = useState(" ");
+  const [date, setDate] = useState(" ");
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <div>
+        <label>Gelir Açıklaması</label>
+        <input type="text" value={description} placeholder="Örneğin: Maaş" />
+      </div>
+      <div>
+        <label>Miktar</label>
+        <input type="number" value={amount} placeholder="Örneğin: 5000" />
+      </div>
+      <div>
+        <label>Tarih</label>
+        <input type="date" value={date} />
+      </div>
+      <button type="submit">Gelir Ekle</button>
+    </form>
+  );
 }
