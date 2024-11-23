2b054b292704b364d494a3010bda9833ac059426
ezgiozturan <ezgiozturan05@gmail.com>
2024-11-23 15:14:55 +0300

    Update incomeSlice.jsx
    createSlice ile incomeSlice kuruldu. reducer olarak addIncome eklendi.

--- budget-tracker/src/redux/incomeSlice.jsx
+++ budget-tracker/src/redux/incomeSlice.jsx
@@ -1,3 +1,19 @@
+import { createSlice } from "@reduxjs/toolkit";
+
 const initialState = {
   incomeList: [],
 };
+
+const incomeSlice = createSlice({
+  name: "incomes",
+  initialState,
+  reducers: {
+    addIncome: (state, action) => {
+      state.incomeList.push(action.payload);
+    },
+    //setIncome eklenecek , localStorage yüklemeyi burada yapalım?
+  },
+});
+
+export default incomeSlice.reducer;
+export const { addIncome } = incomeSlice.action;
