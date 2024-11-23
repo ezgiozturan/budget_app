c8064e1ba3cdcbb3192fe6034c73bf1d7d034df9
ezgiozturan <ezgiozturan05@gmail.com>
2024-11-23 14:44:24 +0300

    IncomeForm
    Form bileşenleri eklendi

--- budget-tracker/src/components/IncomeForm.jsx
+++ budget-tracker/src/components/IncomeForm.jsx
@@ -1,3 +1,18 @@
 export default function IncomeForm() {
-  return <form onSubmit={handleSubmit}></form>;
+  return;
+  <form onSubmit={handleSubmit}>
+    <div>
+      <label>Gelir Açıklaması</label>
+      <input type="text" value={description} placeholder="Örneğin: Maaş" />
+    </div>
+    <div>
+      <label>Miktar</label>
+      <input type="number" value={amount} placeholder="Örneğin: 5000" />
+    </div>
+    <div>
+      <label>Tarih</label>
+      <input type="date" value={date} />
+    </div>
+    <button type="submit">Gelir Ekle</button>
+  </form>;
 }
